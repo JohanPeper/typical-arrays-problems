f1afb3ed5a99f3a2acf48f31a2a86901fd1b67d7
Ivan <70844716+JohanPeper@users.noreply.github.com>
2020-10-02 14:53:56 +0300

    fix: change ... to apply

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-
+Array.prototype.max = function() {
+    return Math.max.apply(null, this);
+  };
+  
+  Array.prototype.min = function() {
+    return Math.min.apply(null, this);
+  };
 exports.min = function min(array) {
 
     if ( array === undefined || array.length == 0 ){
         return 0;}
-    else return Math.min(...array);
+    else return Math.min.apply(null, array)
 }
 
 exports.max = function max(array) {
     if ( array === undefined || array.length == 0 ){
         return 0;}
-    else return Math.max(...array);
+    else return Math.max.apply(null, array);
 }
 
 exports.avg = function avg(array) {
