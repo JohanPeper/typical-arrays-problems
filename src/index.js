c96b6b9f80961ef4fdbb8b358ff3bbbbb7f1f7b0
Ivan <70844716+JohanPeper@users.noreply.github.com>
2020-10-02 13:39:51 +0300

    fix: add {}

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 
 exports.min = function min(array) {
 
-    if ( array === undefined || array.length == 0 )
-        return 0;
+    if ( array === undefined || array.length == 0 ){
+        return 0;}
     else return Math.min(...array);
 }
 
 exports.max = function max(array) {
-    if ( array === undefined || array.length == 0 )
-        return 0;
+    if ( array === undefined || array.length == 0 ){
+        return 0;}
     else return Math.max(...array);
 }
 
 exports.avg = function avg(array) {
-    if ( array === undefined || array.length == 0 )
-        return 0;
+    if ( array === undefined || array.length == 0 ){
+        return 0;}
     else return array.reduce((a, b) => (a + b)) / array.length;
 }
